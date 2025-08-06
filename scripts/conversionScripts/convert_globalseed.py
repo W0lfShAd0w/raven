c27078369474dd4d8aceca1ba7ac0f162023ef89
Rollins <rollnk@sawtooth1.ib0.sawtooth.hpc.inl.gov>
2025-08-06 13:03:58 -0600

    The outdated behavior of having randomUtils initialize the RNG with a hardcoded seed value (e.g. 5489) has been replaced with the default seed value of 'None', which prompts numpy.random to take a high-entropy seed value from the OS (e.g. the system clock). A new subnode was added to <RunInfo> to allow for a globalSeed to be set in RAVEN prior to any code execution, which ensures a user-supplied RNG seed is applied before any RNG calls are made, if desired. Setting this globalSeed value to 5489 was necessary in all test files to ensure backwards compatibility with old gold results.

---
+++ scripts/conversionScripts/convert_globalseed.py
@@ -0,0 +1,44 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import xml.etree.ElementTree as ET
+
+def convert(tree,fileName=None):
+  """
+    Converts input files to include the <globalSeed> argument under <RunInfo>. This enables
+    backwards compatibility following updates made to randomUtils.py.
+    @ In, tree, xml.etree.ElementTree.ElementTree object, the contents of a RAVEN input file
+    @ In, fileName, the name for the raven input file
+    @Out, tree, xml.etree.ElementTree.ElementTree object, the modified RAVEN input file
+  """
+  newTag = 'globalSeed'
+  newValue = 5489
+
+  simulation = tree.getroot()
+  runInfo = simulation.find('RunInfo')
+  if runInfo is not None:
+    elemExists = False
+    for child in runInfo:
+      if child.tag == newTag:
+        elemExists = True
+    if not elemExists:
+      newElem = ET.Element(newTag)
+      newElem.text = str(newValue)
+      runInfo.append(newElem)
+  return tree
+
+if __name__=='__main__':
+  import convert_utils
+  import sys
+  #!convert_utils.standardMain(sys.argv,convert)
+  convert_utils.standardMain(['/home/rollnk/LWRS-PRLO/raven_latest/scripts/conversionScripts/convert_globalseed.py','--tests','--no-rewrite'],convert)
